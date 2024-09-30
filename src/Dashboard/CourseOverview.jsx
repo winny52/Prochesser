c7c3371d4e9bb0445955ea8e04c17cd9b94106cd
eric <ericmuthuipatch22@gmail.com>
2024-10-01 00:49:33 +0300

    styled dashboard folder

--- src/Dashboard/CourseOverview.jsx
+++ src/Dashboard/CourseOverview.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 const CourseOverview = ({ packageName, progress }) => {
   return (
     <section className="course-overview bg-gradient-to-br from-gray-900 via-gray-800 to-black text-white  py-24 rounded-lg shadow-2xl h-full relative overflow-hidden">
-      <h2 className="text-3xl font-semibold text-blue-900 mb-6 text-center">
+      <h2 className="text-3xl font-semibold text-white mb-6 text-center">
         Current Package: {packageName}
       </h2>
       <div className="progress-bar bg-gray-200 w-full h-4 rounded-full mt-6">
         <div
-          className="progress bg-blue-600 h-4 rounded-full transition-all duration-300 ease-in-out"
+          className="progress bg-yellow-500 h-4 rounded-full transition-all duration-300 ease-in-out"
           style={{ width: `${progress}%` }}
         ></div>
       </div>
-      <button className="mt-6 bg-blue-700 text-white py-2 px-6 rounded-full transition duration-300 hover:bg-blue-600 focus:outline-none focus:ring-4 focus:ring-blue-300 w-full md:w-auto">
+      <button className="mt-6 bg-yellow-500 text-white py-2 px-6 rounded-full transition duration-300 hover:bg-yellow-700 focus:outline-none focus:ring-4 focus:ring-yellow-300 w-full md:w-auto">
         Start Your First Lesson
       </button>
     </section>
--- src/Dashboard/Learningpath.jsx
+++ src/Dashboard/Learningpath.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
-const LearningPath =({ modules })=> {
+const LearningPath = ({ modules }) => {
   return (
-    <section className="learning-pathw-full p-6">
-      <h2 className="text-2xl font-bold mb-4">Learning Path</h2>
-      <ul>
+    <section className="learning-path w-full p-6 bg-gray-100 rounded-lg shadow-lg">
+      <h2 className="text-2xl font-bold mb-6 text-blue-900 text-center">Learning Path</h2>
+      <ul className="space-y-6">
         {modules.map((module, index) => (
-          <li key={index} className="mb-4">
-            <h3 className="text-xl">{module.title}</h3>
-            <p>{module.description}</p>
-            <button className="mt-2 bg-blue-600 text-white py-1 px-3 rounded">Start</button>
+          <li key={index} className="p-6 bg-white rounded-lg shadow-md">
+            <h3 className="text-xl font-semibold text-blue-700">{module.title}</h3>
+            <p className="text-gray-700 mt-2">{module.description}</p>
+            <button className="mt-4 bg-yellow-500 text-gray-900 py-2 px-4 rounded-full transition duration-300 hover:bg-yellow-600 focus:outline-none focus:ring-4 focus:ring-yellow-300">
+              Start
+            </button>
           </li>
         ))}
       </ul>
