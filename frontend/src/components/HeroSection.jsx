ee3df64bd700e2d9b3f11f087636fa2f84040abb
eric <ericmuthuipatch22@gmail.com>
2024-10-04 16:40:34 +0300

    modified heroes section

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -6,25 +6,26 @@ const HeroSection = () => {
   const navigate = useNavigate();
 
   return (
-    <section className="pt-32   w-screen h-screen bg-gradient-to-r from-gray-100 to-gray-900 text-white">
+    <section className="py-20 min-h-screen w-screen bg-gradient-to-r from-gray-100 to-gray-900 text-white flex items-center justify-center">
       {/* Content Section */}
-      <div className="relative z-10 flex flex-col md:flex-row items-center justify-between h-full px-6 sm:px-12">
+      <div className="relative z-10 flex flex-col md:flex-row items-center justify-between h-full w-full px-6 sm:px-12">
         
         {/* Left: Text Content */}
-        <div className="flex-1 text-center md:text-left md:pr-10 order-2 md:order-1"> {/* Order change for smaller devices */}
-        <h1 className="text-2xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold leading-tight tracking-tight bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 to-black hover:from-pink-500 hover:to-yellow-500 transition-colors duration-300">
-  Turn Every Checkmate Into a Payday: 
-  <br className="hidden md:block" /> 
-  Compete, Conquer, Cash In
-</h1>
+        <div className="flex-1 text-center md:text-left md:pr-10 order-2 md:order-1">
+          {/* Heading */}
+          <h1 className="text-2xl sm:text-5xl md:text-6xl lg:text-7xl font-extrabold leading-tight tracking-tight bg-clip-text text-transparent bg-gradient-to-r from-yellow-400 to-black hover:from-pink-500 hover:to-yellow-500 transition-colors duration-300">
+            Turn Every Checkmate Into a Payday: 
+            <br className="hidden md:block" /> 
+            Compete, Conquer, Cash In
+          </h1>
 
-
-          <p className="mt-6 text-base font-bold sm:text-lg md:text-l lg:text-xl leading-relaxed md:leading-normal max-w-2xl mx-auto md:mx-0  bg-clip-text text-black">
+          {/* Paragraph */}
+          <p className="mt-6 text-base font-bold sm:text-lg md:text-l lg:text-xl leading-relaxed md:leading-normal max-w-2xl mx-auto md:mx-0 bg-clip-text text-black">
             Immerse yourself in a world where strategy meets reward. At ProChesser, every checkmate isn't just a win on the board; it's a victory for your wallet. Experience the thrill of classic chess with the added excitement of earning with each move.
           </p>
 
           {/* Buttons */}
-          <div className="mt-10 flex flex-col sm:flex-row items-center justify-center sm:justify-start space-y-4 sm:space-y-0 sm:space-x-6">
+          <div className="mt-10 flex flex-col sm:flex-row items-center sm:justify-start space-y-4 sm:space-y-0 sm:space-x-6">
             <button 
               onClick={() => navigate('/signup')} 
               className="bg-yellow-500 hover:bg-yellow-600 text-black font-semibold py-3 px-8 rounded-full shadow-lg transition-transform transform hover:scale-105 focus:outline-none focus:ring-4 focus:ring-yellow-300 focus:ring-opacity-50 hover:text-white font-bold"
@@ -41,7 +42,7 @@ const HeroSection = () => {
         </div>
 
         {/* Right: Image */}
-        <div className="flex-1 w-full md:w-1/2 h-full flex items-center justify-center md:pl-10 order-2 md:order-1"> {/* Order change for smaller devices */}
+        <div className="flex-1 w-full md:w-1/2 h-full flex items-center justify-center md:pl-10 order-1 md:order-2">
           <img
             src={mockupImage}
             alt="Mockup"
