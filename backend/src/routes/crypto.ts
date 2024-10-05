0b5588b8e1023fe8bd313a49bfca198c05296fc1
AMAN SINGH <89124765+Aman1919@users.noreply.github.com>
2024-10-05 21:19:20 +0000

    crypto payment works

--- backend/src/routes/crypto.ts
+++ backend/src/routes/crypto.ts
@@ -1,14 +1,12 @@
 
 import express from "express";
 const {
-  getURL,successTransaction, getId
+  successTransaction, getId
 } = require('../controllers/payments/crypto');
 const {authenticateJWT}=require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
-router.post("/get-url", authenticateJWT, getURL);
-router.post("/success/transaction",successTransaction);
 router.post("/get-wallet-address", authenticateJWT,getId);
 
 export default router;
