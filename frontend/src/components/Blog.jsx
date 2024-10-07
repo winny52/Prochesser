77e7979d18d884766f0db184e2b7c7919a368d48
Winny Chelangat <winnychelangat28@gmail.com>
2024-10-07 17:24:45 +0300

    editted blog component

--- frontend/src/components/Blog.jsx
+++ frontend/src/components/Blog.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
@@ -6,12 +7,24 @@ const Blog = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  useEffect(() => {
+    axios.get('http://localhost/wordpress/wp-json/wp/v2/posts')
+      .then(response => {
+        setPosts(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
 
+  if (loading) return <p>Loading...</p>;
   if (error) return <p>Error loading posts: {error.message}</p>;
 
   return (
-    <div className='w-full'>
-      <h1 className="text-yellow-600 text- center ">  Our Blog Posts</h1>
+    <div w-full mt-12>
+      <h1 classname="text-yellow-600"> Posts</h1>
       <ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
     {posts.map(post => (
       <li key={post.id} className="border p-4 rounded-lg shadow-md">
@@ -26,3 +39,4 @@ const Blog = () => {
 };
 
 export default Blog;
+
